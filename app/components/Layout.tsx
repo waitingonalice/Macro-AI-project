6ad56e5ad94fa7b095a7c1a8d14a37e7fcb2f257
Wilson <fcsie55@gmail.com>
2025-10-22 23:05:00 +0800

    reduce layout font size

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -6,7 +6,7 @@ interface LayoutProps {
 export function Layout({ children, title }: LayoutProps) {
   return (
     <div className="flex flex-col items-center h-screen w-screen bg-[url('/images/background.svg')] p-4 bg-no-repeat bg-center">
-      <h1 className="text-6xl font-bold text-white text-center font-conthrax-sb mt-[0.5%]">
+      <h1 className="text-5xl font-bold text-white text-center font-conthrax-sb mt-[0.5%]">
         {title}
       </h1>
       <div className="m-4 size-full">{children}</div>
